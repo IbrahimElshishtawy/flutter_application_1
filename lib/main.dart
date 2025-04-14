60e5f40cc90ee56bf8973dcca12407958948f72a
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-04-14 17:28:54 +0200

    day 1
    my learn what is meaning the material App , appbar , center text and floatingactionbutton and body

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+  Text text = Text("hello world");
+  //text is a widget that displays a string of text
+  // center widget is used to center the child widget
+  // Scafflod widget
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        // Scaffold is a widget that provides a structure for the app
+        appBar: AppBar(
+          // appBar is a widget that displays a material design app bar
+          title: Text("My Frist App"),
+          // title is a widget that displays a string of text
+          backgroundColor: Color(0xFF6200EE),
         ),
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
+        // floatingActionButton is a widget that displays a button that floats above the content
+        body: Center(child: text),
+        // body is a widget that displays the main content of the app
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+    ),
+  );
+  // runApp is a function that takes a widget and makes it the root of the widget tree
 }
+
+// The main function is the entry point of the application
