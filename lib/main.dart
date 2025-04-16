ac428e5100bb9010aaec275e0fc400fcc46b8214
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-04-17 00:02:19 +0200

    Day 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  //text is a widget that displays a string of text
-  // center widget is used to center the child widget
-  // Scafflod widget
-  runApp(HelloWorld());
-  // runApp is a function that takes a widget and makes it the root of the widget tree
+  runApp(AppPhoto());
 }
 
-// The main function is the entry point of the application
-class HelloWorld extends StatelessWidget {
-  // StatelessWidget is a widget that does not require mutable state
-  List<Widget> mywidgets = [];
+class AppPhoto extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-          centerTitle: true,
+        backgroundColor: Color(0xFF000000),
 
-          backgroundColor: Colors.redAccent,
+        body: Center(
+          child: Image(
+            image: AssetImage("images/me.JPG"),
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+            color: const Color.fromARGB(255, 113, 137, 157).withOpacity(0.5),
+            colorBlendMode: BlendMode.darken,
+          ),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              height: 100,
-              width: 200,
-              color: Colors.lightBlue,
-              child: Center(
-                child: Text(
-                  'Hello World',
-                  style: TextStyle(fontSize: 30, color: Colors.red),
-                ),
-              ),
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.greenAccent,
-              child: Center(
-                child: Text(
-                  'Hello World',
-                  style: TextStyle(fontSize: 30, color: Colors.blue),
-                ),
-              ),
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.lightBlue,
-              child: Container(
-                child: Center(
-                  child: Text(
-                    'Hello World',
-                    style: TextStyle(fontSize: 30, color: Colors.green),
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.lightBlue,
-              child: Center(
-                child: Text(
-                  'Hello World',
-                  style: TextStyle(fontSize: 30, color: Colors.yellow),
-                ),
-              ),
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.lightBlue,
-              child: Center(
-                child: Text(
-                  'Hello World',
-                  style: TextStyle(fontSize: 30, color: Colors.purple),
-                ),
-              ),
-            ),
-          ],
-        ),
+        appBar: AppBar(title: Text("My Photo"), backgroundColor: Colors.blue),
       ),
     );
   }
